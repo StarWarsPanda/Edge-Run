89fbc100b702c0df36b9ba87d2cd81d66e80d978
StarWarsPanda <65609691+StarWarsPanda@users.noreply.github.com>
2021-09-15 11:19:52 -0400

    Update EdgeRun.js

--- EdgeRun.js
+++ EdgeRun.js
@@ -1,4 +1,8 @@
 const level1 = [200,50,750,50,750,350,200,350];
+let keys = [false,false,false,false];
+
+let x = 480;
+let y = 240;
 
 function setup()
 {
@@ -8,10 +12,79 @@ function setup()
 
 function draw()
 {
+  if (keys[0])
+  {
+    x--;
+  }
+  if (keys[1])
+  {
+    x++;
+  }
+  if (keys[2])
+  {
+    y--;
+  }
+  if (keys[3])
+  {
+    y++;
+  }
+  background(0);
+  push();
   stroke(255);
-  for (let i = 0;i < level1.length - 2;i+=2)
+  strokeWeight(2);
+  strokeCap(SQUARE);
+  strokeJoin(MITER);
+  noFill();
+  beginShape();
+  for (let i = 0;i < level1.length;i+=2)
     {
-      line(level1[i],level1[i+1],level1[i+2],level1[i+3]);
+      vertex(level1[i],level1[i+1]);
     }
-  line (level1[level1.length-2],level1[level1.length-1],level1[0],level1[1]);
+  vertex(level1[0],level1[1]-1);
+  endShape();
+  pop();
+  push();
+  noStroke();
+  rect(x,y,50,50);
+  pop();
+}
+
+function keyPressed()
+{
+  if (keyCode == LEFT_ARROW)
+  {
+    keys[0] = true;
+  }
+  if (keyCode == RIGHT_ARROW)
+  {
+    keys[1] = true;
+  }
+  if (keyCode == UP_ARROW)
+  {
+    keys[2] = true;
+  }
+  if (keyCode == DOWN_ARROW)
+  {
+    keys[3] = true;
+  }
+}
+
+function keyReleased()
+{
+  if (keyCode == LEFT_ARROW)
+  {
+    keys[0] = false;
+  }
+  if (keyCode == RIGHT_ARROW)
+  {
+    keys[1] = false;
+  }
+  if (keyCode == UP_ARROW)
+  {
+    keys[2] = false;
+  }
+  if (keyCode == DOWN_ARROW)
+  {
+    keys[3] = false;
+  }
 }
